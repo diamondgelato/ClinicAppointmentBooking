3dbc7881fcc5942c91edc18f4b097ce73db38c78
Mugdha Kurkure <60062557+diamondgelato@users.noreply.github.com>
2021-05-29 04:13:04 +0530

    Revert "Untracking 2 files"
    This reverts commit 1957b5556a2dc10460cf492ea28fcb7685e45bd1.

---
+++ src/ProgramVar.py
@@ -0,0 +1,2 @@
+databasePath = r'C:\Users\ashuk\Documents\Semester 4\Mini Project\ClinicAppointmentBooking\data.db'
+pdfPath = r'C:\Users\ashuk\Documents\Semester 4\Mini Project\ClinicAppointmentBooking\assets\pdfs'
