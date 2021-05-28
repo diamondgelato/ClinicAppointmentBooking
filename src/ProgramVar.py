1957b5556a2dc10460cf492ea28fcb7685e45bd1
Mugdha Kurkure <60062557+diamondgelato@users.noreply.github.com>
2021-05-29 03:40:46 +0530

    Untracking 2 files

--- src/ProgramVar.py
+++
@@ -1,2 +0,0 @@
-databasePath = r'C:\Users\ashuk\Documents\Semester 4\Mini Project\ClinicAppointmentBooking\data.db'
-pdfPath = r'C:\Users\ashuk\Documents\Semester 4\Mini Project\ClinicAppointmentBooking\assets\pdfs'
